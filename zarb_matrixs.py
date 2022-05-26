d27c6dc0433895648669b6dab47a127c2a995f84
aidamzz <aidamohagheghzade@gmail.com>
2022-05-26 20:00:46 +0430

    مثلث خیام

---
+++ zarb_matrixs.py
@@ -0,0 +1,11 @@
+n, m , k = map(int, input().split(' '))
+l1 = []
+for i in range(n):
+    l = map(int, input().split(' '))
+    l1.append(list(l))
+l2 = []
+for i in range(m):
+    l = map(int, input().split(' '))
+    l2.append(list(l))
+
+for i in range(m):

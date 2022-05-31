8c4fd60033ec0c5c07c2468a1a8b2cf21eb93443
aidamzz <aidamohagheghzade@gmail.com>
2022-05-31 19:10:15 +0430

    آخ جون طرف نیست

---
+++ akh_jun_taraf_nist.py
@@ -0,0 +1,26 @@
+
+a = input()
+b = input()
+c = input()
+d = input()
+e = input()
+f = input()
+
+
+g = b.split(" ")
+h = d.split(" ")
+i = f.split(" ")
+
+
+
+
+
+j= set(g + h+ i)
+
+
+
+
+
+k = {'1shanbe', '2shanbe', '3shanbe', '5shanbe', 'shanbe', '4shanbe', 'jome'}
+l = len(k)-len(j)
+print(l)

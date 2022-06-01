3df699aad91c1b6d5a7a7a6e7797283fb55a0acd
aidamzz <aidamohagheghzade@gmail.com>
2022-06-01 17:33:33 +0430

    گراف مکمل(کامل نیست)

---
+++ graph_mokamel.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split(' '))
+l = {}
+
+for i in range(m):
+    k, j = input().split(' ')
+    if k in l.keys():
+        l[k].append({k,j})
+        if j in l.keys():
+            l[j].append({k,j})
+        else:
+            l[j] = [{k,j}]
+    else:
+        l[k] = [{k,j}]
+        if j in l.keys():
+            l[j].append({k,j})
+        else:
+            l[j] = [{k,j}]
+
+m1 = int(input())
+p = []
+
+for i in range(m1):
+    k, j = input().split(' ')
+    if k in l.keys() and ({k,j} in l[k]):
+        p.append('NO')
+    else:
+        p.append('YES')
+
+
+for i in p:
+    print(i)
+
+
+

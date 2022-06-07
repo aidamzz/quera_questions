dfcee83676594168bf0423357dd2c70b29c2416a
aidamzz <aidamohagheghzade@gmail.com>
2022-06-07 22:15:16 +0430

    نیوفلدر (کامل نیست

--- new_folder.py
+++ new_folder.py
@@ -1,21 +1,27 @@
+
 n = int(input())
 folders = [input() for i in range(n)]
 dic_folders = {}
 list_folders = []
+
 for i in folders:
     if i in dic_folders.keys():
+        
         a = "'{} ({})'".format(i,str(dic_folders[i]))
+        b = "{} ({})".format(i,str(dic_folders[i]))
         list_folders.append(a)
         dic_folders[i] +=1
+        dic_folders[b] = 1
     else:
-        if i in folders:
+        if "'" + i + "'" in list_folders:
             a = "'{} ({})'".format(i,str(1))
-            list_folders.append(a)
-            dic_folders[a] =1
-        else:
             dic_folders[i] = 1
-            a = "'{} '".format(i)
             list_folders.append(a)
-    print(list_folders)
-    list_folders = sorted(list_folders)
+        else:
+
+            dic_folders[i] = 1
+            a = "'{}'".format(i)
+            list_folders.append(a)
+
+    list_folders = sorted(list_folders, key=lambda x: ''.join(x.split()))
     print(', '.join(k for k in list_folders))

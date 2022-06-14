7651baeeeeab5d2def9183d0873c566dbce490e9
aidamzz <aidamohagheghzade@gmail.com>
2022-06-14 23:10:04 +0430

    مسابقه الگوریتمی 42

--- Spider-Man_Across_the_Spider-Verse _(2022).py
+++ Spider-Man_Across_the_Spider-Verse _(2022).py
@@ -9,29 +9,28 @@ daraje = int(input())
 
 def tabe45(l,a,b):
     l1 = []
-    if daraje == 45:
-        for i in range(a):
-            j = i
-            k = 0
-            s = ' '*(a-(i+1))
-            while j != -1:
-                s += l[j][k]
-                s += ' '
-                k += 1
-                j -= 1
-            l1.append(s)
-        for i in range(a):
-            j = a-1
-            k = 1+i
-            s = ' '*(i+1)
+    for i in range(a):
+        j = i
+        k = 0
+        s = ' '*(a-(i+1))
+        while j != -1:
+            s += l[j][k]
+            s += ' '
+            k += 1
+            j -= 1
+        l1.append(s)
+    for i in range(a):
+        j = a-1
+        k = 1+i
+        s = ' '*(i+1)
 
-            while k != b :
-                s += l[j][k]
-                s += ' '
-                k += 1
-                j -= 1
+        while k != b :
+            s += l[j][k]
+            s += ' '
+            k += 1
+            j -= 1
 
-            l1.append(s)
+        l1.append(s)
     return l1
 
 def tabe180(l):
@@ -39,13 +38,49 @@ def tabe180(l):
 
 def tabe90(l, a,b):
     l1 = []
-    for j in range(a):
+    for j in range(b):
         s = ''
-        for i in range(b):
+        for i in range(a):
             s += l[i][j]
-        l1.append(s)
+        l1.append(s[::-1])
     return l1
 
+if daraje == 45 :
+    ja = tabe45(l,a,b)
+    for i in ja:
+        print(i)
+if daraje == 90:
+    ja = tabe90(l,a,b)
+    for i in ja:
+        print(i)
+if daraje == 135:
+    ja = tabe180(l,a,b)
+    ja = tabe45(l,a,b)
+    ja = ja[::-1]
+    for i in ja:
+        print(i)
+if daraje == 180:
+    ja = tabe180(l,a,b)
+    for i in ja:
+        print(i)
+if daraje == 225:
+    ja = tabe180(l,a,b)
+    ja = tabe45(l,a,b)
+    for i in ja:
+        print(i)
+elif daraje == 270:
+    ja = tabe90(l,a,b)
+    ja = ja[::-1]
+    for i in ja:
+        print(i)
+if daraje == 315:
+    ja = tabe45(l,a,b)
+    ja = ja[::-1]
+    for i in ja:
+        print(i)
+if daraje == 360:
+    for i in l:
+        print(i)
 
 
 
---
+++ entegham_be_sabke_mamad.py
@@ -0,0 +1,9 @@
+a,b,c,A,B,C,X,Y,T,P = map(int, input().split(' '))
+
+def sood(a,b,c,A,B,C,X,Y,T):
+    aa = A-a
+    bb = B-b
+    cc = C - c
+    if P > 0:
+        if aa == max(aa,bb,cc):
+    

94f0ad74be89ddac411496de241e8c6bc99439e0
aidamzz <aidamohagheghzade@gmail.com>
2022-07-04 23:08:49 +0430

    انتقام به سبک ممد

--- entegham_be_sabke_mamad.py
+++ entegham_be_sabke_mamad.py
@@ -1,9 +1,26 @@
-a,b,c,A,B,C,X,Y,T,P = map(int, input().split(' '))
+price_of_brown_in_trade,price_of_white_in_trade,price_of_magic_in_trade,buyingprice_of_brown,buyingprice_of_white,buyingprice_of_magic,brown,white,magic_count,want = map(int, input().split(' '))
+pul = 0
 
-def sood(a,b,c,A,B,C,X,Y,T):
-    aa = A-a
-    bb = B-b
-    cc = C - c
-    if P > 0:
-        if aa == max(aa,bb,cc):
-    
+while want != 0:
+    soode_ghahveii = buyingprice_of_brown- price_of_brown_in_trade
+    soode_sefid = buyingprice_of_white- price_of_white_in_trade
+    soode_magic = buyingprice_of_magic - price_of_magic_in_trade
+    if magic_count != 0:
+        m = max(soode_ghahveii, soode_magic, soode_sefid)
+    else:
+        m = max(soode_ghahveii, soode_sefid)
+
+    if soode_sefid == m:
+        pul += soode_sefid
+        buyingprice_of_white -=  white
+    elif soode_ghahveii == m:
+        pul += soode_ghahveii
+        buyingprice_of_brown -= brown
+    elif soode_magic == m:
+        if magic_count != 0:
+            pul += soode_magic
+            magic_count -= 1
+
+    want -= 1
+            
+print(pul)    
